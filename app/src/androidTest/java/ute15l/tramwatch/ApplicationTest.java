8f98989227b13b255d7e15b399b44fff5a314e60
proszko1 <pawel.roszkowski@micom.pl>
2015-05-07 15:22:04 +0200

    Initial commit

---
+++ app/src/androidTest/java/ute15l/tramwatch/ApplicationTest.java
@@ -0,0 +1,13 @@
+package ute15l.tramwatch;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
